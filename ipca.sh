a3e25cc2c24698f28507a63e191dc15e4e23a053
Daniel Cason <cason@ic.unicamp.br>
2016-12-15 11:46:10 -0200

    Script IPCA permite consulta de 6 índices de inflação
       * São 6 os índices aceito pela Calculadora do Cidadão, todos eles
         enumerados no 'usage', mostrado por exemplo com: ./ipca.sh -h
       * O código do índice é aceito como valor para o parâmetro -I. O
         valor passado não é verificado, sendo o padrão: 00433IPC-A.

--- ipca.sh
+++ ipca.sh
@@ -12,11 +12,18 @@ usage() {
 	echo -en " [-v valorCorrecao=$valorCorrecao]"
 	echo -en " [-f dataFinal=$dataFinal]"
 	echo -en " [-i dataInicial=$dataInicial]"
-	echo ""
+	echo -en " [-I Indice]\n\n"
+	echo -en "Os índices aceitos para a opção -I são:\n"
+	echo -e " 00189IGP-M\t\tIGP-M (FGV) - a partir de 06/1989"
+	echo -e " 00190IGP-DI\t\tIGP-DI (FGV) - a partir de 02/1944"
+	echo -e " 00188INPC\t\tINPC (IBGE) - a partir de 04/1979"
+	echo -e " 00433IPC-A\t\tIPC-A (IBGE) - a partir de 01/1980 [Indice padrão]"
+	echo -e " 10764IPC-E\t\tIPCA-E (IBGE) - a partir de 01/1992"
+	echo -e " 00191IPC-BRASIL\tIPC-BRASIL (FGV) - a partir de 01/1990"
 	exit 1
 }
 
-while getopts "v:i:f:" opt; do
+while getopts "v:i:f:I:" opt; do
 	case $opt in
 		v)
 			valorCorrecao="$OPTARG"
@@ -26,6 +33,9 @@ while getopts "v:i:f:" opt; do
 			;;
 		f)
 			dataFinal="$OPTARG"
+			;;
+		I)
+			selIndice="$OPTARG"
 			;;
 		\?)
 			usage
