8f27c34934296e48b9563a0e1d9167a523a90cf2
Daniel Cason <cason@ic.unicamp.br>
2016-12-15 11:05:58 -0200

    Script SELIC usa getopts para receber parametros
       * Parâmetros: -v valorCorrecao, -f dataFinal, -i dataInicial
       * Parâmatros sem modificador são agora ignorados.

--- selic.sh
+++ selic.sh
@@ -2,22 +2,41 @@
 
 URL="https://www3.bcb.gov.br/CALCIDADAO/publico/corrigirPelaSelic.do?method=corrigirPelaSelic"
 
-valorCorrecao="$1"
-if [ -z "$valorCorrecao" ] ; then
-	valorCorrecao="1000,00"
-fi
+valorCorrecao="1000,00"
+dataFinal="$(date +%d/%m/%Y)"	# today
+dataInicial="01/01/$(date +%Y)" # first day of the year
+
+usage() {
+	echo -en "Usage: $0"
+	echo -en " [-v valorCorrecao=$valorCorrecao]"
+	echo -en " [-f dataFinal=$dataFinal]"
+	echo -en " [-i dataInicial=$dataInicial]"
+	echo ""
+	exit 1
+}
+
+while getopts "v:i:f:" opt; do
+	case $opt in
+		v)
+			valorCorrecao="$OPTARG"
+			;;
+		i)
+			dataInicial="$OPTARG"
+			;;
+		f)
+			dataFinal="$OPTARG"
+			;;
+		\?)
+			usage
+			;;
+		:)
+			usage
+			;;
+	esac
+done
+
 URL="${URL}&valorCorrecao=$valorCorrecao"
-
-dataInicial="$2"
-if [ -z "$dataInicial" ] ; then
-	dataInicial="01/01/$(date +%Y)" # first day of the year
-fi
 URL="${URL}&dataInicial=$dataInicial"
-
-dataFinal="$3"
-if [ -z "$dataFinal" ] ; then
-	dataFinal="$(date +%d/%m/%Y)"	# today
-fi
 URL="${URL}&dataFinal=$dataFinal"
 
 links2 -dump "$URL"
