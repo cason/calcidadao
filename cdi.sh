884a21ebea6b75b03fab5f94e273229a1ea8c5bc
Daniel Cason <cason@ic.unicamp.br>
2016-12-15 11:13:20 -0200

    Calculadora do Cidadão: script consulta de CDI
       * Script idêntico ao selic.sh, mas que faz consulta de CDI.
       * A principal diferença é a possibilidade de se fazer a consulta
         para um dado percentual do CDI, informado via parâmetro '-p';
         se não informado, o padrão do site que é empregado = 100,00.
       * A data inicial não pode ser o dia atual, ao que parece. O -f
         dataFinal padrão foi então configurado para o dia anterior.

---
+++ cdi.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+URL="https://www3.bcb.gov.br/CALCIDADAO/publico/corrigirPeloCDI.do?method=corrigirPeloCDI"
+
+valorCorrecao="1000,00"
+dataFinal="$(date +%d/%m/%Y -d '-1day')"	# yesterday
+dataInicial="01/01/$(date +%Y)" # first day of the year
+percentualCorrecao= 	# non mandatory, default = 100,00
+
+usage() {
+	echo -en "Usage: $0"
+	echo -en " [-v valorCorrecao=$valorCorrecao]"
+	echo -en " [-f dataFinal=$dataFinal]"
+	echo -en " [-i dataInicial=$dataInicial]"
+	echo -en " [-p percentualCorrecao=100,00]"
+	echo ""
+	exit 1
+}
+
+while getopts "v:i:f:p:" opt; do
+	case $opt in
+		v)
+			valorCorrecao="$OPTARG"
+			;;
+		i)
+			dataInicial="$OPTARG"
+			;;
+		f)
+			dataFinal="$OPTARG"
+			;;
+		p)
+			percentualCorrecao="$OPTARG"
+			;;
+		\?)
+			usage
+			;;
+		:)
+			usage
+			;;
+	esac
+done
+
+URL="${URL}&valorCorrecao=$valorCorrecao"
+URL="${URL}&dataInicial=$dataInicial"
+URL="${URL}&dataFinal=$dataFinal"
+[ -n "$percentualCorrecao" ] && URL="${URL}&percentualCorrecao=$percentualCorrecao"
+
+links2 -dump "$URL"
